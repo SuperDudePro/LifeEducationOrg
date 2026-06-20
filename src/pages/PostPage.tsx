770deba5bc2652e53946048b136fb739e6c529a8
SuperDudePro <william.gayhart@gmail.com>
2026-06-20 16:19:18 -0600

    Update post footer contact link

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -90,7 +90,7 @@ export function PostPage({ slug }: Props) {
       <footer className="footer">
         <div className="footer-inner">
           <div className="footer-text">© LifeEducation.org</div>
-          <a className="footer-link" href="mailto:LifeEducationInformation@gmail.com">LifeEducationInformation@gmail.com</a>
+          <a className="footer-link" href="/contact">Contact</a>
         </div>
       </footer>
     </PageShell>
